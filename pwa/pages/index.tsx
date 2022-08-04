266deaba31f3dcc39997221231f57a96db14d82b
BasileKocheida <77976552+BasileKocheida@users.noreply.github.com>
2022-08-04 14:37:17 +0200

    test complete pipeline

--- pwa/pages/index.tsx
+++ pwa/pages/index.tsx
@@ -31,7 +31,7 @@ const Welcome = () => (
         </div>
         <div className="main__content">
           <h1>
-            Welcome to <strong>My DevOps Project TEST </strong>!
+            Welcome to <strong>My DevOps Project TEST PIPELINE </strong>!
           </h1>
           <div className="main__before-starting">
             <p>
